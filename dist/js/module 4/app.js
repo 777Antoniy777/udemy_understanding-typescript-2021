c30f1bec784443c2125ed1339d60c2e76c0df494
Anton Kuzmitsky <tat7106@gmail.com>
2021-07-27 17:45:42 +0600

    learn combine types and using it

--- dist/js/module 4/app.js
+++ dist/js/module 4/app.js
@@ -1,3 +1,9 @@
 "use strict";
-const fact = 'bla';
+const getAnimal = (obj) => {
+    if ('tigerSpeed' in obj) {
+        return 'tiger';
+    }
+    return 'lion';
+};
+getAnimal({ tigerSpeed: 20, tigerType: '' });
 //# sourceMappingURL=app.js.map
--- src/module 4/app.ts
+++ src/module 4/app.ts
@@ -1,1 +1,23 @@
-const fact = 'bla'
+interface Lion {
+  lionSpeed: string;
+}
+
+interface Tiger {
+  tigerType: string;
+  tigerSpeed: number;
+}
+
+type UAnimal = Lion | Tiger;
+
+// 1) при различных параметрах входа, мы можем выдать результат от наличия элемента в объекте через оператор in (JS оператор)
+// проверить через obj.prop не получится, т.к. TS не знает какой именно тип входит в параметр
+// 2) для классов используется метод JS instanceof
+const getAnimal = (obj: UAnimal): string => {
+  if ('tigerSpeed' in obj) {
+    return 'tiger';
+  }
+
+  return 'lion';
+};
+
+getAnimal({ tigerSpeed: 20, tigerType: ''  });
