0e9d6dc3ed2f490cce27c43e0d570d887799f549
Anton Kuzmitsky <tat7106@gmail.com>
2021-08-16 09:38:19 +0600

    finish learning module 6

--- dist/js/module 6/app.js
+++ dist/js/module 6/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 const Logger = (textMessage) => {
     return (constructor) => {
         console.log(constructor);
@@ -12,10 +15,10 @@ const Logger = (textMessage) => {
     };
 };
 const WithTemplate = (tagName, id) => {
-    return (constructor) => {
+    return (currentConstructor) => {
         const elem = document.querySelector(`#${id}`);
         const createdElem = document.createElement(tagName);
-        const place1 = new constructor('Central Park');
+        const place1 = new currentConstructor('Central Park');
         place1.title = 'New-York Central Park';
         createdElem.textContent = place1.title;
         elem.append(createdElem);
@@ -31,4 +34,61 @@ Place = __decorate([
     Logger('Test Message'),
     WithTemplate('h1', 'template')
 ], Place);
+const Log = (constructor, prop) => {
+    console.log('Property decorator', constructor, prop);
+};
+const Log2 = (constructor, prop, descriptor) => {
+    console.log('Method decorator', constructor, prop, descriptor);
+};
+const Log3 = (constructor, prop, position) => {
+    console.log('Parameter decorator', constructor, prop, position);
+};
+class Product {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    set setPrice(val) {
+        this.price = val;
+    }
+    getTransformedPrice(tax) {
+        return this.price * tax;
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "setPrice", null);
+__decorate([
+    Log2,
+    __param(0, Log3)
+], Product.prototype, "getTransformedPrice", null);
+new Product('', 20);
+const Autobind = (_, _1, descriptor) => {
+    const method = descriptor.value;
+    const updatedDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            return method.bind(this);
+        }
+    };
+    return updatedDescriptor;
+};
+class Printer {
+    constructor() {
+        this.message = 'Some message';
+    }
+    showMessage() {
+        console.log('this from Printer', this);
+        console.log(this.message);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "showMessage", null);
+const printer = new Printer();
+button.addEventListener('click', printer.showMessage);
 //# sourceMappingURL=app.js.map
--- src/module 6/app.ts
+++ src/module 6/app.ts
@@ -1,6 +1,7 @@
 // 1) декораторы применяются к классам во время их объявления, вызывать класс при этом необязательно!
+// использование декораторов возможно при подключении экспериментальной настройки в .tsconfig
 const Logger = (textMessage: string) => {
-  return (constructor: Function) => {
+  return (constructor: any) => {
     console.log(constructor);
     console.log(textMessage);
   };
@@ -10,10 +11,10 @@ const Logger = (textMessage: string) => {
 // мы можем свободно вызывать сам конструктор внутри декоратора и использовать его методы/свойства
 // также даже если св-ва/методы класса защищены, мы можем их использовать или даже менять внутри декоратора
 const WithTemplate = (tagName: string, id: string) => {
-  return (constructor: any) => {
+  return (currentConstructor: any) => {
     const elem = document.querySelector(`#${id}`) as HTMLElement;
     const createdElem = document.createElement(tagName);
-    const place1 = new constructor('Central Park');
+    const place1 = new currentConstructor('Central Park');
     place1.title = 'New-York Central Park';
 
     createdElem.textContent = place1.title;
@@ -28,3 +29,81 @@ class Place {
     this.title = title;
   }
 }
+
+// -- 2 --
+
+// 3) мы также можем применять декоратор к отдельным св-вам или методам класса
+const Log = (constructor: any, prop: string) => {
+  console.log('Property decorator', constructor, prop)
+}
+
+// 4) для методов также есть 3 параметр - descriptor
+const Log2 = (constructor: any, prop: string, descriptor: PropertyDescriptor) => {
+  console.log('Method decorator', constructor, prop, descriptor)
+}
+
+// 5) для параметров функций 3 параметр - position
+const Log3 = (constructor: any, prop: string, position: number) => {
+  console.log('Parameter decorator', constructor, prop, position)
+}
+
+class Product {
+  @Log
+  title: string;
+  private price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+
+  @Log2
+  set setPrice(val: number) {
+    this.price = val;
+  }
+
+  @Log2
+  getTransformedPrice(@Log3 tax: number): number {
+    return this.price * tax;
+  }
+}
+
+new Product('', 20);
+
+// -- 3 --
+
+// 7) декоратор связывает метод с будущим объектом получаемым из класса
+// и таким образом мы уже не зависим от контекста листенера
+const Autobind = (_: any, _1: string, descriptor: PropertyDescriptor) => {
+  const method = descriptor.value;
+
+  // 8) присваиваем новый метод get в обновленном descriptor и даем новый контекст
+  // и возвращаем descriptor
+  const updatedDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return method.bind(this);
+    }
+  }
+
+  return updatedDescriptor;
+};
+
+class Printer {
+  message = 'Some message';
+
+  @Autobind
+  showMessage() {
+    console.log('this from Printer', this)
+    console.log(this.message);
+  }
+}
+
+const printer = new Printer();
+
+// 6) при вызове метода класса в качестве листенера, мы сталкиваемся с тем, что
+// this присваивается объекту до точки и в данном случае это button
+// и в методе showMessage у нас будет результатом undefined
+// чтобы это исправить нужно либо применить метод bind напрямую, либо создать декоратор
+button.addEventListener('click', printer.showMessage);
